0a83d0da9dc01d5a7495b1963e791a040aa0163d
Senthuran Sivananthan <senthuran.sivananthan@microsoft.com>
2017-06-07 20:32:07 -0400

    Added script to find unattached MDs

---
+++ ManagedDisks/utils/find-unattached-disks.ps1
@@ -0,0 +1,14 @@
+﻿$ResourceGroups = Get-AzureRmResourceGroup
+
+Write-Host("Checking, please wait")
+foreach ($ResourceGroup in $ResourceGroups)
+{
+    $Disks = Get-AzureRmDisk -ResourceGroupName $ResourceGroup.ResourceGroupName `
+                | Where { $_.OwnerId -eq $null }
+    
+    foreach ($Disk in $Disks)
+    {
+        Write-Host($ResourceGroup.ResourceGroupName + " - " + $Disk.Name)
+    }
+}
+Write-Host("Done")
