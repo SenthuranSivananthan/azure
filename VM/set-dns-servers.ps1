dc1ada0d693503e0bbd3d7ea91b4b9340865966a
Senthuran Sivananthan <senthuran.sivananthan@microsoft.com>
2017-08-22 20:39:18 -0400

    Set DNS Servers on VM by VM Name

---
+++ VM/set-dns-servers.ps1
@@ -0,0 +1,20 @@
+﻿Add-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionName Lab
+
+$ResourceGroupName = "test-vm"
+$VMName = "vm1"
+
+$VM = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName
+
+ForEach ($NetworkInterface in $VM.NetworkProfile.NetworkInterfaces)
+{
+    $NICName = (Get-AzureRmResource -ResourceId $NetworkInterface.Id).Name
+
+    $NIC = Get-AzureRmNetworkInterface -ResourceGroupName $ResourceGroupName -Name $NICName
+    $NIC.DnsSettings.DnsServers.Add("8.8.4.4")
+    $NIC.DnsSettings.DnsServers.Add("8.8.8.8")
+
+    $NIC | Set-AzureRmNetworkInterface
+}
+
+Restart-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName
