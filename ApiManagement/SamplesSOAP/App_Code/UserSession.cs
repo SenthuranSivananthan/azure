8ae093d07be721e3cd08a5ca4af5d94e6155d04e
Senthuran Sivananthan <senthuran.sivananthan@microsoft.com>
2017-10-03 12:39:50 -0400

    Sample chained SOAP requests

---
+++ ApiManagement/SamplesSOAP/App_Code/UserSession.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for UserSession
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class UserSession : System.Web.Services.WebService
+{
+
+    public UserSession()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string Session()
+    {
+        return Guid.NewGuid().ToString();
+    }
+
+}
