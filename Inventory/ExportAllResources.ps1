c443a22c7f9c777356201b4cb184bb8a9bd71bd7
Senthuran Sivananthan <senthuran.sivananthan@microsoft.com>
2017-11-27 14:46:31 -0500

    Generate inventory for all subscriptions

---
+++ Inventory/ExportAllResources.ps1
@@ -0,0 +1,17 @@
+﻿Login-AzureRmAccount
+
+$LocationToSave = "c:\temp\"
+$DateFormat = Get-Date -Format o | foreach {$_ -replace ":", "."}
+$Subscriptions = Get-AzureRmSubscription
+
+ForEach ($Subscription in $Subscriptions)
+{
+    $SubscriptionId = $Subscription.SubscriptionId
+    $SubscriptionName = $Subscription.Name
+
+    "Generating export for $SubscriptionName ($SubscriptionId)"
+
+    Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+    Get-AzureRmResource | Export-Csv "$LocationToSave\AzureResourceExtract-$SubscriptionId-$SubscriptionName-$DateFormat.csv"
+}
+
