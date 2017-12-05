8d1812f333141853c6604e14917977278d30ac1b
Senthuran Sivananthan <senthuran.sivananthan@microsoft.com>
2017-12-05 11:53:54 -0500

    Extract role assignments

---
+++ Inventory/export-rbac-roles.ps1
@@ -0,0 +1,14 @@
+﻿Login-AzureRmAccount
+
+$LocationToSave = "c:\temp\"
+$DateFormat = Get-Date -Format o | foreach {$_ -replace ":", "."}
+$Subscriptions = Get-AzureRmSubscription
+
+Foreach ($Subscription in $Subscriptions)
+{
+    $SubscriptionId = $Subscription.SubscriptionId
+    $SubscriptionName = $Subscription.Name
+
+    Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+    Get-AzureRmRoleAssignment | Export-Csv "$LocationToSave\AzureRBACExtract-$SubscriptionId-$SubscriptionName-$DateFormat.csv"
+}
