a9be0fa8c678e5d9b19b59f0ced59308d107420f
Senthuran Sivananthan <senthuran.sivananthan@microsoft.com>
2018-05-02 14:51:53 -0400

    configure apache

--- AZ/az-httpd-tomcat/install-apache2.sh
+++ AZ/az-httpd-tomcat/install-apache2.sh
@@ -1,12 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y apache2
+sudo apt-get install -y prips
 
 a2enmod proxy_http
 a2enmod proxy_balancer
 a2enmod lbmethod_byrequests
 
-service apache2 restart
+# Update Apache 2 configuration
+BalancerMembers=""
+for ip in `prips $1 $2`
+do
+        BalancerMembers+=$(echo -ne "\n\r\t")
+        BalancerMembers+="BalancerMember http://${ip}:8080/"
+done
 
+cat >/etc/apache2/sites-available/000-default.conf <<EOF
+<Proxy balancer://mycluster>
+   ${BalancerMembers}
+</Proxy>
+
+<VirtualHost *:80>
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+
+        ProxyPass / balancer://mycluster/
+</VirtualHost>
+EOF
+
+service apache2 restart
